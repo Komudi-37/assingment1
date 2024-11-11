99892f5f7ca032247d3867f41705242e8bedc797
Komudi-37 <rkomudi@gmail.com>
2024-11-11 13:57:08 +0530

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -1,4 +1,4 @@
-3) WAP to subtract two integers without using Minus (-) operator. (Hint Bitwise operator)
+//3) WAP to subtract two integers without using Minus (-) operator. (Hint Bitwise operator)
 #include<stdio.h>
 int main(){
     int a,b,sum;
